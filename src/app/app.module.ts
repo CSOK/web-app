9820e23ab774bbb89cfdea44a58489b6388bc2c5
brianokinyi <brianokinyi.bo@gmail.com>
2018-02-13 17:29:29 +0300

    Added what component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WhoComponent } from './who/who.component';
+import { WhatComponent } from './what/what.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    WhoComponent
+    WhoComponent,
+    WhatComponent
   ],
   imports: [
     BrowserModule
