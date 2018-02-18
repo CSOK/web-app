9855a0a53571d4a34a6dd3142637e1b0ac6c64ce
brianokinyi <brianokinyi.bo@gmail.com>
2018-02-18 17:46:55 +0300

    registerd who/history and who/leaders components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WhoComponent } from './who/who.component';
+import { HistoryComponent } from './who/history/history.component';
+import { LeadersComponent } from './who/leaders/leaders.component';
 import { WhatComponent } from './what/what.component';
 import { StoriesComponent } from './stories/stories.component';
 import { PicturesComponent } from './pictures/pictures.component';
@@ -8,6 +10,8 @@ import { PicturesComponent } from './pictures/pictures.component';
 export const AppRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'who', component: WhoComponent },
+    { path: 'who/history', component: HistoryComponent },
+    { path: 'who/leaders', component: LeadersComponent },
     { path: 'what', component: WhatComponent },
     { path: 'stories', component: StoriesComponent },
     { path: 'pictures', component: PicturesComponent },
