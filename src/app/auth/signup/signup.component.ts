8ea6083b130df3b7a243e970dbc27974af9ac83d
brianokinyi <brianokinyi.bo@gmail.com>
2018-02-28 08:19:54 +0300

    Added validation to all singup fields

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -24,5 +24,14 @@ export class SignupComponent implements OnInit {
   get firstName(){
     return this.form.get('firstName');
   }
+  get lastName(){
+    return this.form.get('lastName');
+  }
+  get email(){
+    return this.form.get('email');
+  }
+  get password(){
+    return this.form.get('password');
+  }
 
 }
