8313c093e76402b3b4a60123d5f2a4ed43b17126
brianokinyi <brianokinyi.bo@gmail.com>
2018-03-04 02:18:08 +0300

    Changed stylesheet ext to scss

--- src/app/what/what.component.ts
+++ src/app/what/what.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-what',
   templateUrl: './what.component.html',
-  styleUrls: ['./what.component.css']
+  styleUrls: ['./what.component.scss']
 })
 export class WhatComponent implements OnInit {
 
